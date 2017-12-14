6c817e32d09c3c935c74ea0fe923b0858991230e
Dominic Ma <dominic.c.ma@gmail.com>
2017-12-13 18:42:51 -0800

    Serving index.html

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const db = require('./../db/config');
+const path = require('path');
 
 const app = express();
+
+// Serve up static files
+app.use(express.static(path.join(__dirname, '../client/public/dist')));
 
 app.listen(3000, () => {
   console.log('Listening on localhost:3000');
